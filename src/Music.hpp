aceae9e56f3ff86c7102b2cd89bf02cc6c3bdbaf
jbraine <jbraine@ens-lyon.fr>
2015-03-17 16:54:40 +0100

    Added Melody

--- src/Music.hpp
+++ src/Music.hpp
@@ -139,8 +139,8 @@ private :
 private :
     std::vector<WeightedNote> notes;
 public :
-    using const_iterator = std::vector<WeightedNote>::const_iterator;
-    using iterator = std::vector<WeightedNote>::iterator;
+    using ConstIterator = std::vector<WeightedNote>::const_iterator;
+    using Iterator = std::vector<WeightedNote>::iterator;
     bool check() const;
     void save(std::ostream& o) const;
     static Figure load(std::istream& i);
@@ -158,10 +158,11 @@ public :
     ~Figure() = default;
 
 
-    iterator begin();
-    const_iterator begin() const;
-    iterator end();
-    const_iterator end() const;
+    Iterator begin();
+    ConstIterator begin() const;
+    Iterator end();
+    ConstIterator end() const;
+    unsigned nbNotes() const;
 
     void addNote(const Note& n, unsigned weight = 1);
 
@@ -239,10 +240,44 @@ private:
     {
         unsigned pos; ///< Position inside the progession
     };
+    using PositionFunction = std::function<unsigned(const Figure&, const Figure&, unsigned)>;
 private:
     std::vector<PositionedFigure> figures;
     Key k;
     Signature s;
+public :
+    using Iterator = std::vector<PositionedFigure>::iterator;
+    using ConstIterator = std::vector<PositionedFigure>::const_iterator;
+
+    bool check() const;
+    void save(std::ostream& o) const;
+    static Melody load(std::istream& i);
+    static Melody randomInstance();
+    std::ostream& operator<<(std::ostream& o) const;
+    std::ostream& operator>>(std::istream& i);
+    static Melody fromStream(std::istream& i);
+
+    Melody(const Key& k, const Signature& s);
+    template<typename Iterator>
+    Melody(const Key& k, const Signature& s, Iterator begin, Iterator end, const PositionFunction& pf
+           = PositionFunction([](const Figure&, const Figure&, unsigned i){return (i+1)%4;}));
+    Melody(const Melody&) = default;
+    Melody(Melody&&) = default;
+    Melody& operator=(const Melody&) = default;
+    ~Melody() = default;
+
+    Iterator begin();
+    ConstIterator begin() const;
+    Iterator end();
+    ConstIterator end() const;
+    unsigned nbFigures();
+
+    const Key& getKey() const;
+    const Signature& getSignature() const;
+
+    void addFigure(const Figure& f, const PositionFunction& pf = PositionFunction([](const Figure&, const Figure&, unsigned i){return (i+1)%4;}));
+
+    Melody transpose(const Key& newKey) const;
 };
 
 /**
