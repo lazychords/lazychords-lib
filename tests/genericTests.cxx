8a575d191ef602a1fa91e30082c967c005043bb5
Julien <julien.braine@ens-lyon.fr>
2015-06-05 11:43:07 +1000

    Added what to do in generic tests

---
+++ tests/genericTests.cxx
@@ -0,0 +1,43 @@
+/**Generic verifications :
+
+Note :I will be implementing functions such as IsHashable<T>::value, IsComparable<T>::value, ...
+
+For Hashable classes, all instances means for all ids, for the others it means for all random instances tested.
+When needing to test strings, there will be a function testStringBegin() and a function testStingEnd() that will iterate on all strings to test.
+
+Of course, do not forget that these are generic tests, you still have to write tests to try out all your functions
+
+*Test for all classes*
+
+- All instances are valid -> Try check on them
+- Default constructor creates a valid class
+- Operator >> never fails (nor throws)
+- Function save never fails
+
+*Test for comparable classes*
+- For all instances, operator =, copy constructor, move constructor, ==, != are coherent
+
+*Test for hashable classes*
+-All hashable classes are comparable/loadable/<<
+-If created from an invalid ID, fromID fails
+-id(fromId(x))) = x
+-fromId(id(c)) == c
+-forall x <> y, fromId(x) != fromId(y)
+-for all tries on randomInstance, id(randomInstance) is valid
+
+*Tests for all loadable classes*
+
+-load(save) = Identity (for comparable classes)
+-if load fails/throws, the stream is not modified, if load succeeds, save(load) = Identity (load is not meant to be a human readable format)
+
+*Tests for << classes*
+
+- >>(<<) = Identity (for comparable classes)
+-fromStream(in) = in >> DefaultConstructor or (throws an exception and >> fails)
+- if << fails, the stream is not modified.
+
+**/
+
+
+
+
