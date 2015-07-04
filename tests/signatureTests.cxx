62152d2cc6530b5c67e356c3a44371ddfacec337
alcinos <carion.nicolas@gmail.com>
2015-07-04 17:32:45 -0600

    test custom constructor signature

--- tests/signatureTests.cxx
+++ tests/signatureTests.cxx
@@ -8,6 +8,7 @@
 #include <map>
 #include <algorithm>
 
+#define FOR(i,n) for(unsigned i=0;i<n;i++)
 using namespace std;
 
 
@@ -18,5 +19,29 @@ TEST_CASE("Test of Signature structure","Signature")
         REQUIRE(S.getNumber() == 4);
         REQUIRE(S.getStep() == 4);
     }
-
+    
+    SECTION("Custom Constructor"){
+        SECTION("Valid Constructions"){
+            FOR(i,8){
+                FOR(j,15){
+                    REQUIRE_NOTHROW(Signature(j+1,1<<i));
+                }
+            }
+            REQUIRE_NOTHROW(Signature(4,4));
+        }
+        SECTION("Invalid Constructions"){
+            FOR(i,33){
+                REQUIRE_THROWS(Signature(0,i));
+                REQUIRE_THROWS(Signature(i,0));
+                REQUIRE_THROWS(Signature(i,1));
+                REQUIRE_THROWS(Signature(i,3));
+                REQUIRE_THROWS(Signature(i,5));
+                REQUIRE_THROWS(Signature(i,6));
+                REQUIRE_THROWS(Signature(i,7));
+                REQUIRE_THROWS(Signature(i,9));
+                REQUIRE_THROWS(Signature(i,10));
+                REQUIRE_THROWS(Signature(i,12));
+            }
+        }
+    }
 }
