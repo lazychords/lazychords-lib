11f0708edbf24d047994324942b2776d7d1605e5
Julien <julien.braine@ens-lyon.fr>
2015-07-17 17:45:37 +1000

    Started documentation for Concepts

--- src/Concepts.hpp
+++ src/Concepts.hpp
@@ -31,12 +31,22 @@ namespace Concepts
 {
 
 /**
- *@struct template<typename C> struct HasId
- *@brief toto2
+ *@class HasId
+ *@brief Allows for static and dynamic testing of the Id Concept
+ *@pre One of the two following conditions must be followed :
+ *- C has none of the functions id, maxId, fromId
+ *- C follows the IdConcept which is described @ref ConceptId "here".
+ *@author Julien
 **/
 template<typename C>
 struct HasId
 {
+    /**
+     *@brief is true if C follows the Id concept (described @ref ConceptId "here").
+     *@author Julien
+     *@todo test for check and randomInstance
+     *@todo hide the value of value in the documentation
+    **/
     static constexpr bool value = impl::HasIdImpl<C, std::is_class<C>::value>::value;
 
     /**
@@ -44,6 +54,7 @@ struct HasId
      *@brief If HasId<C>::value = true, checks if C::id, C::maxId, C::fromId respect the dynamic part of the concept id concept. Otherwise does not do anything.
      *
      *@details Respecting the Id concept means :
+     *- Having all the functions needed for all that follows
      *- From x = 0 to maxId -1 we have :
      *  -# fromId(x) never throws
      *  -# We have check(fromId(x))
@@ -55,6 +66,7 @@ struct HasId
      *
      *@throw never throws
      *@note this function is equivalent to a test.
+     *@author Julien
     **/
     static void test();
 };
