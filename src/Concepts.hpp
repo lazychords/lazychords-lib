ad5ec3d30ec0d69a01835b6619165bf390298d27
Julien <julien.braine@ens-lyon.fr>
2015-07-17 15:51:04 +1000

    Starting doc for common functions

--- src/Concepts.hpp
+++ src/Concepts.hpp
@@ -10,12 +10,31 @@ ENABLE_CALLABLE_TEST(fromId)
 ENABLE_CALLABLE_TEST(randomInstance)
 
 namespace impl {}
-namespace Concepts
-{
 
+namespace impl {
 template<typename C, bool b>
 struct HasIdImpl;
 
+template<typename C, bool b>
+struct HasRandomInstanceImpl;
+}
+
+namespace Concepts
+{
+template<typename C>
+struct HasId
+{
+    static constexpr bool value = impl::HasIdImpl<C, std::is_class<C>::value>::value;
+};
+
+template<typename C>
+struct HasRandomInstance
+{
+    static constexpr bool value = impl::HasRandomInstanceImpl<C, std::is_class<C>::value>::value;
+};
+}
+
+namespace impl {
 template<typename C>
 struct HasIdImpl<C, true>
 {
@@ -30,15 +49,6 @@ struct HasIdImpl<C, false>
 };
 
 template<typename C>
-struct HasId
-{
-    static constexpr bool value = HasIdImpl<C, std::is_class<C>::value>::value;
-};
-
-template<typename C, bool b>
-struct HasRandomInstanceImpl;
-
-template<typename C>
 struct HasRandomInstanceImpl<C, true>
 {
     static constexpr bool value = IS_CALLABLE_MEMBER(randomInstance, C (C::*) ());
@@ -48,12 +58,6 @@ template<typename C>
 struct HasRandomInstanceImpl<C, false>
 {
     static constexpr bool value = false;
-};
-
-template<typename C>
-struct HasRandomInstance
-{
-    static constexpr bool value = HasRandomInstanceImpl<C, std::is_class<C>::value>::value;
 };
 
 }
--- src/Music.hpp
+++ src/Music.hpp
@@ -2,6 +2,19 @@
 #ifndef MUSIC_HPP_INCLUDED
 #define MUSIC_HPP_INCLUDED
 
+/**
+ *@file
+ *@brief File that includes the whole BaseMusic Module
+**/
+
+/**
+ *@fn template<typename T> bool T::check() const
+ *@pre This function should exist for any T in the public interface of the BaseMusic Module
+ *@throw This function never throws
+ *@return true if the invariants are respected
+ *@note this function is meant to be used in the following way : ASSERT(check())
+**/
+
 #include "Chord.hpp"
 #include "Figure.hpp"
 #include "Key.hpp"
