c08e4e2b75a6a8adf66df4a848a7106c3db814d8
alcinos <carion.nicolas@gmail.com>
2015-07-18 19:37:32 -0600

    create genericTests.hpp and corresponding documentation page

--- tests/genericTests.cxx
+++ tests/genericTests.cxx
@@ -1,47 +1,3 @@
-#include "catch.hpp"
-#include "../src/Concepts.hpp"
-#include "Pitch.hpp"
-#include "Figure.hpp"
-/**Generic verifications :
-
-Note :I will be implementing functions such as IsHashable<T>::value, IsComparable<T>::value, ...
-
-For Hashable classes, all instances means for all ids, for the others it means for all random instances tested.
-When needing to test strings, there will be a function testStringBegin() and a function testStingEnd() that will iterate on all strings to test.
-
-Of course, do not forget that these are generic tests, you still have to write tests to try out all your functions
-
-*Test for all classes*
-
-- All instances are valid -> Try check on them
-- Default constructor creates a valid class
-- Operator >> never fails (nor throws)
-- Function save never fails
-
-*Test for comparable classes*
-- For all instances, operator =, copy constructor, move constructor, ==, != are coherent
-
-*Test for hashable classes*
--All hashable classes are comparable/loadable/<<
--If created from an invalid ID, fromID fails
--id(fromId(x))) = x
--fromId(id(c)) == c
--forall x <> y, fromId(x) != fromId(y)
--for all tries on randomInstance, id(randomInstance) is valid
-
-*Tests for all loadable classes*
-
--load(save) = Identity (for comparable classes)
--if load fails/throws, the stream is not modified, if load succeeds, save(load) = Identity (load is not meant to be a human readable format)
-
-*Tests for << classes*
-
-- >>(<<) = Identity (for comparable classes)
--fromStream(in) = in >> DefaultConstructor or (throws an exception and >> fails)
-- if << fails, the stream is not modified.
-
-**/
-
 //#define GENERIC_TEST(T) TEST_CASE("Generic Tests for #T", "[#T]"){generic_tests<T, "#T">::go();}
 
 template<typename C, typename Test, typename = typename std::enable_if<Concepts::HasId<C>::value>::type >
@@ -64,14 +20,3 @@ void forAll(const Test& test, unsigned n = 1000)
     }
 }
 
-///Just testing if test framework works
-TEST_CASE("Testing test framework","[testFramework]")
-{
-    SECTION("success"){
-        //forAll<Pitch>([](const Pitch& p){REQUIRE(p.check());});
-        //forAll<Figure>([](const Figure& f){REQUIRE(f.check());});
-    }
-    SECTION("fail"){
-	REQUIRE_FALSE(0 > 1);
-    }
-}
---
+++ tests/genericTests.hxx
@@ -0,0 +1,54 @@
+/**\page page1 Generic verifications
+\tableofcontents
+
+This page describes all the generic verifications that can be made on our classes.
+This verifications are made to ensure consistency of the api as well as correctness of the implementation.
+
+In the following, "all instances" means all ids in the context of hashable classes, and for the others it means for all random instances tested (we generate a fixed number of them, by default 1000).
+
+When needing to test strings, there will be a function testStringBegin() and a function testStingEnd() that will iterate on all strings to test.
+
+Of course, do not forget that these are generic tests, you still have to write tests to try out all your functions
+
+\section section1 Test for all classes
+
+- All instances are valid -> Try check on them
+- Default constructor creates a valid class
+- Operator >> never fails (nor throws)
+- Function save never fails
+
+\section section2 Test for comparable classes
+- For all instances, operator =, copy constructor, move constructor, ==, != are coherent
+
+\section section3 Test for hashable classes
+- All hashable classes are comparable/loadable/<<
+- If created from an invalid ID, fromID fails
+- id(fromId(x))) = x
+- fromId(id(c)) == c
+- forall x <> y, fromId(x) != fromId(y)
+- for all tries on randomInstance, id(randomInstance) is valid
+
+\section section4 Tests for all loadable classes
+
+- load(save) = Identity (for comparable classes)
+- if load fails/throws, the stream is not modified, if load succeeds, save(load) = Identity (load is not meant to be a human readable format)
+
+\section section5 Tests for << classes
+
+- >>(<<) = Identity (for comparable classes)
+-fromStream(in) = in >> DefaultConstructor or (throws an exception and >> fails)
+- if << fails, the stream is not modified.
+
+**/
+
+
+/**@brief This utility function applies the given test to all possible elements of a given class 
+ * @param test The test function
+ * @param C Type of the class to test
+ * @param n If enumerations of all possible values of the class is impossible, this value controls the number of random instances that must be generated
+ */
+template<typename C, typename Test>
+void forAll(const Test& test, unsigned n);
+
+
+#include "genericTests.cxx"
